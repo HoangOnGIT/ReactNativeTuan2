ff940c6fcd441da15a94df2ab15e6f0aa06fb32c
H82M19\Student <Student>
2025-08-27 07:38:05 +0700

    Bai 3 + Bai 4 + Bai 5

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // b1.then(() => {}).catch(err => console.log(err)
 // )
 //B2
-let b2 = new Promise((resolve) => {
-    return setTimeout(() => resolve(12), 1000);
+// let b2 = new Promise<number>((resolve) => {
+//     return setTimeout(() => resolve(12), 1000);
+// })
+// b2.then(data => console.log(data)).catch((err) => console.log(err))
+let b3 = new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error("Some thing went wrong")), 1000);
 });
-b2.then(data => console.log(data)).catch((err) => console.log(err));
+b3.then(data => console.log(data)).catch((err) => console.log(err));
+function simulateTask(time) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve("task done"), time);
+    });
+}
+simulateTask(2000).then(data => console.log(data)).catch((err) => console.log(err));
+const arr = [simulateTask(1000), simulateTask(1500), simulateTask(1250)];
+Promise.all(arr).then(values => console.log(values)).catch(err => console.log(err));
--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,29 @@
 
 //B2
 
-let b2 = new Promise<number>((resolve) => {
-    return setTimeout(() => resolve(12), 1000);
+// let b2 = new Promise<number>((resolve) => {
+//     return setTimeout(() => resolve(12), 1000);
+// })
+
+// b2.then(data => console.log(data)).catch((err) => console.log(err))
+
+let b3 = new Promise<string>((resolve, reject) => {
+    setTimeout(() => reject(new Error("Some thing went wrong")), 1000)
 })
 
-b2.then(data => console.log(data)).catch((err) => console.log(err))
+b3.then(data => console.log(data)).catch((err) => console.log(err))
+
+
+function simulateTask(time: number) {
+    return new Promise<string>(resolve => {
+        setTimeout(() => resolve("task done"), time)
+    })
+}
+
+simulateTask(2000).then(data => console.log(data)).catch((err) => console.log(err));
+
+const arr = [simulateTask(1000), simulateTask(1500), simulateTask(1250)]
+
+
+Promise.all(arr).then(values => console.log(values)).catch(err => console.log(err)
+)
