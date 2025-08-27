1eed44c59a8aef192768fb9d81dc8eadef6a5145
H82M19\Student <Student>
2025-08-27 07:52:23 +0700

    Bai 7 + Bai 8 + Bai 9 + Bai 10

--- dist/index.js
+++ dist/index.js
@@ -13,15 +13,42 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //     return setTimeout(() => resolve(12), 1000);
 // })
 // b2.then(data => console.log(data)).catch((err) => console.log(err))
-let b3 = new Promise((resolve, reject) => {
-    setTimeout(() => reject(new Error("Some thing went wrong")), 1000);
-});
-b3.then(data => console.log(data)).catch((err) => console.log(err));
-function simulateTask(time) {
+// //Bai 3
+// let b3 = new Promise<string>((resolve, reject) => {
+//     setTimeout(() => reject(new Error("Some thing went wrong")), 1000)
+// })
+// b3.then(data => console.log(data)).catch((err) => console.log(err))
+// //bai 5
+// function simulateTask(time: number) {
+//     return new Promise<string>(resolve => {
+//         setTimeout(() => resolve("task done"), time)
+//     })
+// }
+// simulateTask(2000).then(data => console.log(data)).catch((err) => console.log(err));
+// //bai 6
+// const arr = [simulateTask(1000), simulateTask(1500), simulateTask(1250)]
+// Promise.all(arr).then(values => console.log(values)).catch(err => console.log(err))
+function simulateTaskPrintTime(time) {
     return new Promise(resolve => {
-        setTimeout(() => resolve("task done"), time);
+        setTimeout(() => resolve(time), time);
     });
 }
-simulateTask(2000).then(data => console.log(data)).catch((err) => console.log(err));
-const arr = [simulateTask(1000), simulateTask(1500), simulateTask(1250)];
-Promise.all(arr).then(values => console.log(values)).catch(err => console.log(err));
+//bai 7 
+const arrTime = [simulateTaskPrintTime(1000), simulateTaskPrintTime(1500), simulateTaskPrintTime(1250)];
+Promise.race(arrTime).then(value => console.log(value)).catch(err => console.log(err));
+const b8 = new Promise(resolve => {
+    setTimeout(() => resolve(2), 1000);
+});
+//bai 8 
+b8.then(value => value * value)
+    .then(value => value * 2)
+    .then(value => value + 5)
+    .then(value => console.log(value))
+    .catch(err => console.log(err));
+const b9 = new Promise(resolve => {
+    setTimeout(() => resolve([1, 2, 3, 4, 5, 6, 7].filter(num => num % 2 == 0)), 1000);
+});
+//bai 9 + 10
+b9.then(values => console.log(values))
+    .catch(err => console.log(err))
+    .finally(() => console.log("Done"));
--- src/index.ts
+++ src/index.ts
@@ -17,23 +17,51 @@
 
 // b2.then(data => console.log(data)).catch((err) => console.log(err))
 
-let b3 = new Promise<string>((resolve, reject) => {
-    setTimeout(() => reject(new Error("Some thing went wrong")), 1000)
-})
+// //Bai 3
+// let b3 = new Promise<string>((resolve, reject) => {
+//     setTimeout(() => reject(new Error("Some thing went wrong")), 1000)
+// })
 
-b3.then(data => console.log(data)).catch((err) => console.log(err))
+// b3.then(data => console.log(data)).catch((err) => console.log(err))
 
+// //bai 5
+// function simulateTask(time: number) {
+//     return new Promise<string>(resolve => {
+//         setTimeout(() => resolve("task done"), time)
+//     })
+// }
 
-function simulateTask(time: number) {
-    return new Promise<string>(resolve => {
-        setTimeout(() => resolve("task done"), time)
+// simulateTask(2000).then(data => console.log(data)).catch((err) => console.log(err));
+
+// //bai 6
+// const arr = [simulateTask(1000), simulateTask(1500), simulateTask(1250)]
+// Promise.all(arr).then(values => console.log(values)).catch(err => console.log(err))
+
+function simulateTaskPrintTime(time: number) {
+    return new Promise<number>(resolve => {
+        setTimeout(() => resolve(time), time)
     })
 }
 
-simulateTask(2000).then(data => console.log(data)).catch((err) => console.log(err));
+//bai 7 
+const arrTime = [simulateTaskPrintTime(1000), simulateTaskPrintTime(1500), simulateTaskPrintTime(1250)]
+Promise.race(arrTime).then(value => console.log(value)).catch(err => console.log(err))
 
-const arr = [simulateTask(1000), simulateTask(1500), simulateTask(1250)]
 
+const b8 = new Promise<number>(resolve => {
+    setTimeout(() => resolve(2), 1000)
+})
+//bai 8 
+b8.then(value => value * value)
+    .then(value => value * 2)
+    .then(value => value + 5)
+    .then(value => console.log(value))
+    .catch(err => console.log(err))
 
-Promise.all(arr).then(values => console.log(values)).catch(err => console.log(err)
-)
+const b9 = new Promise<number[]>(resolve => {
+    setTimeout(() => resolve([1,2,3,4,5,6,7].filter(num => num%2 ==0)), 1000)
+})
+//bai 9 + 10
+b9.then(values => console.log(values))
+    .catch(err => console.log(err))
+    .finally(() => console.log("Done"))
